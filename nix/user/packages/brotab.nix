8b4734b6d968db27a14f25371ac79f08a385a76b
Paul Victor Raj <paulvictor@gmail.com>
2021-09-20 09:47:56 +0530

    Fix brotab's python version issues

--- nix/user/packages/brotab.nix
+++ nix/user/packages/brotab.nix
@@ -5,7 +5,7 @@ self: super:
     postInstall =
       ''
         ls $out/lib
-      sed -i "s|\$PWD/brotab_mediator.py|$out/bin/bt_mediator|" $out/lib/python3.8/site-packages/brotab/mediator/*json
+      sed -i "s|\$PWD/brotab_mediator.py|$out/bin/bt_mediator|" $out/lib/python*/site-packages/brotab/mediator/*json
       sed -i "s|\$PWD/brotab_mediator.py|$out/bin/bt_mediator|" $out/config/*json
       '';
   });
