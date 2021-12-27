590705e2f09d2442797282c49cf046e97b871547
Paul Victor Raj <paulvictor@gmail.com>
2021-12-27 14:28:53 +0530

    WIP :Sarge config

--- nix/root/devices/sarge/default.nix
+++ nix/root/devices/sarge/default.nix
@@ -1,5 +1,5 @@
 # TODO : Look at https://github.com/openlab-aux/vuizvui for a better arrangement
-# { config, pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   # This value determines the NixOS release with which your system is to be
@@ -14,7 +14,25 @@
   imports = [ ./hardware-configuration.nix ];
   boot.loader.systemd-boot.enable = false;
   boot.loader.efi.efiSysMountPoint = "/boot";
+  boot.kernelPackages = pkgs.linuxPackages_5_15;
+
+  services.logind.extraConfig = ''
+    IdleAction=lock
+    IdleActionSec=300
+    HoldoffTimeoutSec=5
+  '';
+
   #boot.initrd.postDeviceCommands = pkgs.lib.mkAfter ''
   #  zfs rollback -r master/local/root@blank
   #'';
+  sops.defaultSopsFile = ./secrets.yaml;
+
+  sops.gnupg.sshKeyPaths = [ "/tomb/${config.networking.hostName}/ssh/ssh_host_rsa_key" ];
+  sops.secrets = {
+    crypt-mount-key = {
+      key = "home-persistence/key";
+      owner = config.users.users.viktor.name;
+      mode = "0400";
+    };
+  };
 }
