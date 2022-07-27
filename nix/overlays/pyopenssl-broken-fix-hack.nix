2a06dcd9ab9767ccd64125ba33c6cb1444cedc41
Paul Victor Raj <paulvictor@gmail.com>
2022-07-27 11:37:55 +0530

    Removed patch for pyopenssl fix

--- nix/overlays/pyopenssl-broken-fix-hack.nix
+++ nix/overlays/pyopenssl-broken-fix-hack.nix
@@ -1,17 +1,31 @@
-final: prev:
+# final: prev:
 
 # https://github.com/NixOS/nixpkgs/issues/175875
-let
-  isSilicon = prev.stdenv.hostPlatform.isDarwin
-    && prev.stdenv.hostPlatform.isAarch64;
-in
-if isSilicon
-then {
-  python3 = prev.python3.override {
-    packageOverrides = python-final: python-prev: {
-      pyopenssl = python-prev.pyopenssl.overrideAttrs (oldAttrs: {
-        meta.broken = false;
-      });
-    };
-  };
-} else { }
+_: { python3, lib, ... }: {
+#   python3Packages = (python3.override {
+#     packageOverrides = _: { urllib3, ... }: {
+#       urllib3 = urllib3.overrideAttrs ({ passthru, ... }: {
+#         passthru = passthru // {
+#           optional-dependencies =
+#             lib.filterAttrs (n: _v: n != "secure")
+#             passthru.optional-dependencies;
+#         };
+#         propagatedBuildInputs = passthru.optional-dependencies.brotli;
+#       });
+#     };
+#   }).pkgs;
+}
+# let
+#   isSilicon = prev.stdenv.hostPlatform.isDarwin
+#     && prev.stdenv.hostPlatform.isAarch64;
+# in
+# if isSilicon
+# then {
+#   python3 = prev.python3.override {
+#     packageOverrides = python-final: python-prev: {
+#       pyopenssl = python-prev.pyopenssl.overrideAttrs (oldAttrs: {
+#         meta.broken = false;
+#       });
+#     };
+#   };
+# } else { }
