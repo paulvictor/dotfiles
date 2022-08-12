5dd5bd01c29100146ee5d5c82e4d5ac7df6fba18
Paul Victor Raj <paulvictor@gmail.com>
2022-08-12 12:25:33 +0530

    mkdir the ssh hostkey path

--- nix/root/modules/ssh.nix
+++ nix/root/modules/ssh.nix
@@ -1,27 +1,45 @@
-{ config, lib, pkgs, ... } :
+args@{ config, lib, pkgs, ... } :
 
-
+let
+  mkHostKeyDirPath = {config, pkgs, ...}:
+    let
+      hostKeyPath = "/tomb/${config.networking.hostName}/ssh/ssh_host_rsa_key";
+    in
+    {
+      system.build.mkHostKeyDirPath = pkgs.runCommandLocal "etc" {} ''
+        set -euo pipefail
+        mkdir -pv $(dirname ${hostKeyPath})
+      '';
+    };
+  sshConfig = { config, lib, pkgs, ... }:
+    let
+      hostKeyPath = "/tomb/${config.networking.hostName}/ssh/ssh_host_rsa_key";
+    in
+    {
+      services.openssh = {
+        enable = true;
+        forwardX11 = true;
+        permitRootLogin = lib.mkDefault "yes";
+        gatewayPorts = "yes";
+        authorizedKeysCommand =
+          with pkgs;
+          let
+            script =
+              writeShellScript
+                "getMyKey"
+                "${curl}/bin/curl -L https://github.com/paulvictor.keys";
+          in lib.mkDefault (toString script); # Google cloud images may want to override this
+        hostKeys =
+          [
+            {
+              path = hostKeyPath;
+              type = "rsa";
+              bits = 4096;
+            }
+          ];
+      };
+    };
+in
 {
-  services.openssh = {
-    enable = true;
-    forwardX11 = true;
-    permitRootLogin = lib.mkDefault "yes";
-    gatewayPorts = "yes";
-    authorizedKeysCommand =
-      with pkgs;
-      let
-        script =
-          writeShellScript
-            "getMyKey"
-            "${curl}/bin/curl -L https://github.com/paulvictor.keys";
-      in lib.mkDefault (toString script); # Google cloud images may want to override this
-    hostKeys =
-      [
-        {
-          path = "/tomb/${config.networking.hostName}/ssh/ssh_host_rsa_key";
-          type = "rsa";
-          bits = 4096;
-        }
-      ];
-  };
+  imports = [ mkHostKeyDirPath sshConfig ];
 }
