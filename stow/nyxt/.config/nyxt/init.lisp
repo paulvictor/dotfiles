5aaebfd2f3c3283c780e5dcb84129ffb41a2bc57
Paul Victor Raj <paulvictor@gmail.com>
2022-09-22 20:04:40 +0530

    Nyxt config - WIP

--- stow/nyxt/.config/nyxt/init.lisp
+++ stow/nyxt/.config/nyxt/init.lisp
@@ -24,7 +24,6 @@
 
 (define-configuration browser
     ((session-restore-prompt :always-restore)
-     (nyxt/web-mode:hints-alphabet "asdfghjkl")
      (autofills '((make-autofill :name "First Name" :fill "Paul Victor")
                   (make-autofill :name "Last Name" :fill "Raj")
                   (make-autofill :name "Name" :fill "Paul Victor Raj")
@@ -59,56 +58,31 @@
 (define-command-global pvr/open-new-tab ()
   "Open a new tab, prompting for the URL"
   (set-url-new-buffer :prefill-current-url-p nil))
-(define-configuration base-mode
-  ((keymap-scheme
+
+(define-command-global pvr/open-url ()
+  "Open a new tab, prompting for the URL"
+  (set-url :prefill-current-url-p nil))
+
+;; (define-configuration nyxt/hint-mode:hint-mode
+;;   ((auto-follow-hints-p t)))
+
+(define-configuration nyxt/web-mode:web-mode
+  ((nyxt/web-mode:hints-alphabet "asdfghjkl")
+   (keymap-scheme
     (define-scheme (:name-prefix "vi-ex" :import %slot-default%)
       scheme:vi-normal
       (list
-       "M-:" 'eval-expression
+ ;;        "M-:" 'eval-expression
+       "r" 'reload-current-buffer
+       "R" 'reload-buffers
        "J" 'switch-buffer-previous
        "K" 'switch-buffer-next
-       "; y" 'nyxt/web-mode:copy-hint-url
-       "; d" 'nyxt/web-mode:download-hint-url
+       "C-c y" 'nyxt/web-mode:copy-hint-url
+       "C-c d" 'nyxt/web-mode:download-hint-url
        "x" 'delete-current-buffer
        "b" 'switch-buffer
        "t" 'pvr/open-new-tab
+       "T" 'set-url-new-buffer
+       "o" 'pvr/open-url
+       "v" 'visual-mode
        "C-x k" 'delete-current-buffer)))))
-
-(define-configuration nyxt/web-mode:web-mode
-  ((keymap-scheme
-    (define-scheme (:name-prefix "web" :import %slot-default%)
-      scheme:emacs
-      (list
-       "C-c y"    'autofill
-       "M-s M-l"  'nyxt/web-mode:search-buffer
-       "M-s M-L"  'nyxt/web-mode:search-buffers
-       "M-x"      'execute-command
-       "M-g M-v"  'hint-mpv
-       "M-V"      'youtube-play-current-page
-       "C-c l"    'org-protocol
-       "C-c c"    'org-capture
-       "C-x M-s"  'start-slynk
-       "C-i"      'nyxt/input-edit-mode:input-edit-mode
-       "C-w"      'nyxt/input-edit-mode:delete-backwards-word
-       "C-f"      'nyxt/input-edit-mode:cursor-forwards
-       "C-b"      'nyxt/input-edit-mode:cursor-backwards
-       "M-f"      'nyxt/input-edit-mode:cursor-forwards-word
-       "M-b"      'nyxt/input-edit-mode:cursor-backwards-word
-       "C-d"      'nyxt/input-edit-mode:delete-forwards
-       "M-d"      'nyxt/input-edit-mode:delete-forwards-word)))))
-
-;; Start with vi and emacs mode
-;; J - previous buffer
-;; K - next buffer
-;; H - jusp history
-;; L - Forward history
-;; f - Follow url
-;; F - follow url in another buffer
-;; x - kill-current-buffer
-;; / search
-;; n - next search
-;; N - prev search
-;; C-x / - search all buffers
-;; C-Space - command mode
-;; C-d/f - Scroll down
-;; C-u/b - Scroll up
