23615bd2dd0d1ee5b3e342b8347c9d2636a5e499
Paul Victor Raj <paulvictor@gmail.com>
2023-03-13 15:29:50 +0530

    Changes in darwin config

--- nix/darwin/services/tuns.nix
+++ nix/darwin/services/tuns.nix
@@ -4,7 +4,7 @@ let
   programWrapper = import ./common.nix { inherit pkgs; };
   tun-0 = "0.0.0.0:8080:localhost:2224";
   tun-1 = "127.0.0.1:5900:localhost:5900";
-  command = tun: ''${pkgs.openssh}/bin/ssh -NT -i /Users/paul/custom-ssh/temp-key -R ${tun} -l viktor paulvictor.xyz'';
+  command = tun: ''${pkgs.openssh}/bin/ssh -NT -i /Users/paul/custom-ssh/temp-key -R ${tun} -l ghost paulvictor.xyz -o StrictHostKeyChecking=off'';
 in
 {
   launchd.daemons = {
