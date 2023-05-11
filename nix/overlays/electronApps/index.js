04ec52e7864e77a93997c4956ff7bebd4f851b0a
Paul Victor Raj <paulvictor@gmail.com>
2023-05-11 20:31:10 +0530

    Update the electron apps' user agent

--- nix/overlays/electronApps/index.js
+++ nix/overlays/electronApps/index.js
@@ -5,6 +5,7 @@ const path = require('path');
 var args = process.argv.slice(2);
 var appUrl = args[0];
 var appName = args[1];
+var userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36";
 app.on('ready', () => {
   var mainWindow = new BrowserWindow(
   {
@@ -20,11 +21,13 @@ app.on('ready', () => {
     }
   });
   mainWindow.on("page-title-updated", (ev, title) => {
-    mainWindow.setTitle(title);
+      console.log(ev);
+    mainWindow.setTitle(appName);
   });
-  mainWindow.loadURL(appUrl,
-    {
-      userAgent: "Mozilla/5.0 (X11; Linux x86_64; rv:92.0) Gecko/20100101 Firefox/92.0"
-    });
+  mainWindow.loadURL(appUrl, {userAgent: userAgent}).then(() => {
+      console.log("loadurl");
+      console.log(mainWindow.getTitle());
+    mainWindow.setTitle(appName);
+  });
   mainWindow.on('closed', () => { mainWindow = null; });
 });
