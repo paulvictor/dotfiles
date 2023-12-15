4d00f76866429809b0acc364f4958a40589d22be
Paul Victor Raj <paulvictor@gmail.com>
2023-12-15 18:55:04 +0530

    Added miso cache

--- nix/root/caches.nix
+++ nix/root/caches.nix
@@ -3,8 +3,14 @@
 {
   nix.settings.substituters = [
     "https://nix-community.cachix.org"
+    "https://miso-haskell.cachix.org"
+  ];
+  nix.settings.trusted-substituters = [
+    "https://nix-community.cachix.org"
+    "https://miso-haskell.cachix.org"
   ];
   nix.settings.trusted-public-keys = [
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "miso-haskell.cachix.org-1:6N2DooyFlZOHUfJtAx1Q09H0P5XXYzoxxQYiwn6W1e8="
   ];
 }
