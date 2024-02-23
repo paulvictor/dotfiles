0a777d0e6cf7ef45abd112f14c2db1931c01a2de
Paul Victor Raj <paulvictor@gmail.com>
2024-02-23 19:00:30 +0530

    Nyxt config changes

--- stow/nyxt/.config/nyxt/config.lisp
+++ stow/nyxt/.config/nyxt/config.lisp
@@ -22,67 +22,67 @@
 
 (setf (uiop:getenv "GTK_THEME") "Adwaita:dark")
 
-(define-configuration browser
-    ((session-restore-prompt :always-restore)
-     (autofills '((make-autofill :name "First Name" :fill "Paul Victor")
-                  (make-autofill :name "Last Name" :fill "Raj")
-                  (make-autofill :name "Name" :fill "Paul Victor Raj")
-                  (make-autofill :name "Email" :fill "paulvictor@gmail.com")))
-     (external-editor-program (or (uiop/os:getenv "EDITOR") "emacsclient -c"))))
+;; (define-configuration browser
+;;     ((session-restore-prompt :always-restore)
+;;      (autofills '((make-autofill :name "First Name" :fill "Paul Victor")
+;;                   (make-autofill :name "Last Name" :fill "Raj")
+;;                   (make-autofill :name "Name" :fill "Paul Victor Raj")
+;;                   (make-autofill :name "Email" :fill "paulvictor@gmail.com")))
+;;      (external-editor-program (or (uiop/os:getenv "EDITOR") "emacsclient -c"))))
+
+;; (define-configuration buffer
+;;   ((default-modes (append '(emacs-mode) %slot-default%))
+;;    (override-map
+;;     (let ((map (make-keymap "override-map")))
+;;       (define-key map
+;;         "J"   'switch-buffer-previous
+;;         "K"   'switch-buffer-next
+;; ;;         "; y" 'nyxt/web-mode:copy-hint-url
+;; ;;         "; d" 'nyxt/web-mode:download-hint-url
+;;         "x"   'delete-current-buffer
+;;         "b"   'switch-buffer
+;;         "M-x" 'execute-command)))))
 
 (define-configuration buffer
-  ((default-modes (append '(emacs-mode) %slot-default%))
-   (override-map
-    (let ((map (make-keymap "override-map")))
-      (define-key map
-        "J"   'switch-buffer-previous
-        "K"   'switch-buffer-next
-;;         "; y" 'nyxt/web-mode:copy-hint-url
-;;         "; d" 'nyxt/web-mode:download-hint-url
-        "x"   'delete-current-buffer
-        "b"   'switch-buffer
-        "M-x" 'execute-command)))))
+  ((default-modes
+    (pushnew 'nyxt/mode/emacs:emacs-mode %slot-value%))))
 
-(define-configuration web-buffer
-  ((default-modes `(nyxt/emacs-mode:emacs-mode vi-normal-mode
-                    ,@%slot-default%))))
+;; (define-configuration prompt-buffer
+;;   ((keymap-scheme-name scheme:emacs)
+;;    (override-map
+;;     (let ((map (make-keymap "prompt-map")))
+;;       (define-key map
+;;         "C-j" 'nyxt/prompt-buffer-mode:select-next
+;;         "C-k" 'nyxt/prompt-buffer-mode:select-previous)))))
 
-(define-configuration prompt-buffer
-  ((keymap-scheme-name scheme:emacs)
-   (override-map
-    (let ((map (make-keymap "prompt-map")))
-      (define-key map
-        "C-j" 'nyxt/prompt-buffer-mode:select-next
-        "C-k" 'nyxt/prompt-buffer-mode:select-previous)))))
+;; (define-command-global pvr/open-new-tab ()
+;;   "Open a new tab, prompting for the URL"
+;;   (set-url-new-buffer :prefill-current-url-p nil))
 
-(define-command-global pvr/open-new-tab ()
-  "Open a new tab, prompting for the URL"
-  (set-url-new-buffer :prefill-current-url-p nil))
+;; (define-command-global pvr/open-url ()
+;;   "Open a new tab, prompting for the URL"
+;;   (set-url :prefill-current-url-p nil))
 
-(define-command-global pvr/open-url ()
-  "Open a new tab, prompting for the URL"
-  (set-url :prefill-current-url-p nil))
+;; ;; (define-configuration nyxt/hint-mode:hint-mode
+;; ;;   ((auto-follow-hints-p t)))
 
-;; (define-configuration nyxt/hint-mode:hint-mode
-;;   ((auto-follow-hints-p t)))
-
-(define-configuration nyxt/web-mode:web-mode
-  ((nyxt/web-mode:hints-alphabet "asdfghjkl")
-   (keymap-scheme
-    (define-scheme (:name-prefix "vi-ex" :import %slot-default%)
-      scheme:vi-normal
-      (list
- ;;        "M-:" 'eval-expression
-       "r" 'reload-current-buffer
-       "R" 'reload-buffers
-       "J" 'switch-buffer-previous
-       "K" 'switch-buffer-next
-       "C-c y" 'nyxt/web-mode:copy-hint-url
-       "C-c d" 'nyxt/web-mode:download-hint-url
-       "x" 'delete-current-buffer
-       "b" 'switch-buffer
-       "t" 'pvr/open-new-tab
-       "T" 'set-url-new-buffer
-       "o" 'pvr/open-url
-       "v" 'visual-mode
-       "C-x k" 'delete-current-buffer)))))
+;; (define-configuration nyxt/web-mode:web-mode
+;;   ((nyxt/web-mode:hints-alphabet "asdfghjkl")
+;;    (keymap-scheme
+;;     (define-scheme (:name-prefix "vi-ex" :import %slot-default%)
+;;       scheme:vi-normal
+;;       (list
+;;  ;;        "M-:" 'eval-expression
+;;        "r" 'reload-current-buffer
+;;        "R" 'reload-buffers
+;;        "J" 'switch-buffer-previous
+;;        "K" 'switch-buffer-next
+;;        "C-c y" 'nyxt/web-mode:copy-hint-url
+;;        "C-c d" 'nyxt/web-mode:download-hint-url
+;;        "x" 'delete-current-buffer
+;;        "b" 'switch-buffer
+;;        "t" 'pvr/open-new-tab
+;;        "T" 'set-url-new-buffer
+;;        "o" 'pvr/open-url
+;;        "v" 'visual-mode
+;;        "C-x k" 'delete-current-buffer)))))
