c5ef4140942b73b5943b1310cccb5458dc3a206c
Paul Victor Raj <paulvictor@gmail.com>
2024-09-20 18:53:21 +0530

    Changed dunst to have a different color theme for sway keybindings

--- nix/userland/config/dunstrc.nix
+++ nix/userland/config/dunstrc.nix
@@ -1,24 +1,30 @@
 {
   global = {
     font                 = "SauceCodePro Nerd Font Mono 10";
-    allow_markup         = "yes";
-    plain_text           = "no";
     format               = "%s\\n%b";
     transparency         = "40";
     ignore_newline       = "no";
     show_indicators      = "yes";
     separator_color      = "frame";
+    frame_color = "#8caaee";
     sort                 = "yes";
+    enable_posix_regex = "yes";
     alignment            = "left";
-    bounce_freq          = "0";
+
     word_wrap            = "yes";
     indicate_hidden      = "yes";
     show_age_threshold   = "60";
     idle_threshold       = "120";
-    geometry             = "600x100-0+30";
+
+    width = "(0, 300)";
+    height = "100";
+    offset = "50x30";
+    origin = "top-right";
+    corner_radius = "6";
+
     shrink               = "no";
     line_height          = "0";
-    notification_height  = "100";
+#     notification_height  = "100";
     separator_height     = "2";
     padding              = "16";
     horizontal_padding   = "12";
@@ -27,36 +33,31 @@
     sticky_history       = "yes";
     history_length       = "20";
     icon_position        = "left";
-    startup_notification = "false";
-  };
-
-  frame = {
-    width = 1;
-    color = "#93a1a1";
-  };
-
-  shortcuts = {
-    close     = "ctrl+space";
-    close_all = "ctrl+shift+space";
-    history   = "ctrl+grave";
-    context   = "ctrl+shift+period";
+#     startup_notification = "false";
   };
 
   urgency_low = {
-    background = "#586e75";
-    foreground = "#eee8d5";
+    background = "#303446";
+    foreground = "#c6d0f5";
     timeout = 10;
   };
 
   urgency_normal = {
-    background = "#073642";
-    foreground = "#eee8d5";
+    background = "#303446";
+    foreground = "#c6d0f5";
     timeout = 5;
   };
 
   urgency_critical = {
-    background = "#dc322f";
-    foreground = "#eee8d5";
+    background = "#303446";
+    foreground = "#c6d0f5";
+    frame_color = "#ef9f76";
     timeout = 0;
+  };
+  # the v_ suffix makes sure it is the last part of the list
+  v_sway-keymap = {
+    background = "#eff1f5";
+    foreground = "#4c4f69";
+    category = ".*sway.keymap.*";
   };
 }
