07fd1f75ade0ce769e5efddc2ef6058fb8c7a55b
Paul Victor Raj <paulvictor@gmail.com>
2025-01-13 19:31:19 +0530

    WIP himalaya integration

--- nix/userland/himalaya.nix
+++ nix/userland/himalaya.nix
@@ -14,34 +14,34 @@ let
     in "${app}/bin/juspay-password-cmd"; # lib.getExe' app "juspay-password-cmd";
 in
 {
-  programs.notmuch = {
-    enable = true;
-    new.tags = [ "new" "unread" ];
-    # hooks.postNew =
-#       let
-#         notmuch-rules = pkgs.writeText "tagmails.nm" ''
-#           -new path:/Trash/
-#           -new path:/Sent/
-#           -new path:/Spam/
-#           +inbox path:/INBOX/
-#           +sent  from:paul.victor@juspay.in
-#           +to-me to:paul.victor@juspay.in and tag:new
-#           +meetings subject:(Invitation: paul.victor@juspay.in) and tag:new
-#           +meetings subject:(Canceled event paul.victor@juspay.in) and tag:new
-#           +docs subject:"Invitation to edit" and tag:new
-#           +ignored -new -inbox from:support@juspay.in and tag:new
-#           +ignored -new -inbox from:no-reply@juspay.in and tag:new
-#           +ignored -new -inbox to:analytics@juspay.in and tag:new
-#           +ignored -new -inbox to:monitor@juspay.in and tag:new
-#           +ooo -new -inbox subject:/OOO/ and tag:new
-#           -new tag:new
-#         '';
-#       in ''
-#         # Run on first import
-#         # notmuch tag -unread -- tag:unread -- to mark existing mails as not unread
-#         ${config.programs.notmuch}/bin/notmuch tag --batch --input=${notmuch-rules}
-#       '';
-  };
+  # programs.notmuch = {
+#     enable = true;
+#     new.tags = [ "new" "unread" ];
+#     # hooks.postNew =
+# #       let
+# #         notmuch-rules = pkgs.writeText "tagmails.nm" ''
+# #           -new path:/Trash/
+# #           -new path:/Sent/
+# #           -new path:/Spam/
+# #           +inbox path:/INBOX/
+# #           +sent  from:paul.victor@juspay.in
+# #           +to-me to:paul.victor@juspay.in and tag:new
+# #           +meetings subject:(Invitation: paul.victor@juspay.in) and tag:new
+# #           +meetings subject:(Canceled event paul.victor@juspay.in) and tag:new
+# #           +docs subject:"Invitation to edit" and tag:new
+# #           +ignored -new -inbox from:support@juspay.in and tag:new
+# #           +ignored -new -inbox from:no-reply@juspay.in and tag:new
+# #           +ignored -new -inbox to:analytics@juspay.in and tag:new
+# #           +ignored -new -inbox to:monitor@juspay.in and tag:new
+# #           +ooo -new -inbox subject:/OOO/ and tag:new
+# #           -new tag:new
+# #         '';
+# #       in ''
+# #         # Run on first import
+# #         # notmuch tag -unread -- tag:unread -- to mark existing mails as not unread
+# #         ${config.programs.notmuch}/bin/notmuch tag --batch --input=${notmuch-rules}
+# #       '';
+#   };
 
   accounts.email.accounts = {
     "juspay.in" = {
@@ -77,7 +77,7 @@ in
           Sent from ${lib.getExe config.programs.himalaya.package}
         '';
       };
-      notmuch.enable = true;
+#       notmuch.enable = true;
       imapnotify = {
         enable = true;
         boxes = [ "INBOX" ];
