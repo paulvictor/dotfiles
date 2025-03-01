714304fa476461bf74d45950efbbc629324ca677
Paul Victor Raj <paulvictor@gmail.com>
2025-03-01 13:56:31 +0530

    Adding good git defaults

--- nix/userland/config/gitconfig.nix
+++ nix/userland/config/gitconfig.nix
@@ -1,11 +1,14 @@
 { config, pkgs, specialArgs, ... }:
-
+# With help from https://blog.gitbutler.com/how-git-core-devs-configure-git/
 {
+  programs.git-worktree-switcher = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+  };
   programs.git = {
     enable = true;
     package = pkgs.gitAndTools.gitFull;
-
-
     aliases = {
       br = ''branch'';
       cb = ''"!echo \"$(git rev-parse --abbrev-ref HEAD 2>/dev/null)\""'';
@@ -25,19 +28,50 @@
       unstage = ''reset HEAD --'';
       up = ''"!git pull origin \"$(git rev-parse --abbrev-ref HEAD)\""'';
     };
+
     diff-so-fancy.enable = true;
-#     merge.conflictstyle = "diff3";
+
     extraConfig = {
       core = {
         whitespace = "trailing-space,space-before-tab";
-        editor = "nvim";
+        editor = "emacsClient -c -n";
       };
+      apply = {
+        whitespace = "fix";
+      };
+      fetch = {
+        prune = true;
+        pruneTags = true;
+        all = true;
+      };
+      rebase = {
+        autoSquash = true;
+        autoStash = true;
+        updateRefs = true;
+      };
+      push = {
+        default = "simple";
+        autoSetupRemote = true;
+        followTags = true;
+      };
+      merge.conflictstyle = "zdiff3";
       color.ui = true;
+      column.ui = "auto";
       pull.rebase = true;
       format.pretty = "format:%C(yellow)%h %Cblue%>(12)%ad %Cgreen%<(7)%aN%Cred%d %Creset%s";
-      help.autocorrect = 1;
+      help.autocorrect = "prompt";
       init.defaultBranch = "main";
-
+      branch.sort = "-committerdate";
+      tag.sort = "version.refname";
+      commit.verbose = true;
+      rerere.enable = true;
+      rerere.autoupdate = true;
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
+      };
     };
     signing.key = "3264D1648F8FB1A4A3F74C9318497AC961BB2FB6";
     userEmail = "paulvictor@gmail.com";
