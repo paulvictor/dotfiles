c4a64c3c2e2d33652a53a9cbc8e7d067aa253867
Paul Victor Raj <paulvictor@gmail.com>
2025-07-17 10:55:54 +0530

    Update schemesh

--- nix/overlays/schemesh.nix
+++ nix/overlays/schemesh.nix
@@ -1,30 +1,14 @@
 final: prev:
 
 with final;
-{
-  schemesh = stdenv.mkDerivation {
-    name = "schemesh";
-    nativeBuildInputs = [ chez makeWrapper ];
-    buildInputs = [ zlib ncurses libuuid ];
-    src = final.fetchFromGitHub {
-      owner = "cosmos72";
-      repo = "schemesh";
-      rev = "2a454dfcd40d9dd576dae91ebc32b528b98170ed";
-      hash = "sha256-Y8jEGn4hkM3Xk0MFcFEil9+AIn3EHZBGiEUywf7oyPI=";
-    };
-    buildPhase = ''
-     mkdir -pv $out/bin
-     cp -a $src/* .
-
-     DESTDIR=$out prefix= SCHEMESH_LIBDIR=/bin make -e install
-     wrapProgram $out/bin/schemesh --add-flags "--library-dir $out/bin"
-  '';
-    phases = [ "buildPhase" ];
-    meta = {
-      mainProgram = "schemesh";
-      description = "A Unix shell and Lisp REPL, fused together";
-      homepage = "https://github.com/cosmos72/schemesh";
-      license = lib.licenses.gpl2;
-    };
+let
+  src = final.fetchFromGitHub {
+    owner = "cosmos72";
+    repo = "schemesh";
+    rev = "d74e17a8cebbf3e55d6d16775e31b49478f9b4cc";
+    hash = "sha256-j1LLggESfzIWfgiym7T1SU4hdIYqgN1luBBVztRcIFM=";
   };
+  package = callPackage ("${src}/default.nix") {};
+in {
+  schemesh = package;
 }
