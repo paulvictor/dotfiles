6f28edc4617f05b7848d0646ed4571f1d1664a68
Paul Victor Raj <paulvictor@gmail.com>
2026-02-09 16:43:30 +0530

    Remove sorlag

--- nix/userland/devices/sorlag.nix
+++ nix/userland/devices/sorlag.nix
@@ -1,9 +1,4 @@
 {lib,...}:
 {
-  services.batteryAlert.enable = false;
-  wayland.windowManager.sway.config.output = {
-    "HDMI-A-1" = lib.mkForce {
-      mode = "2560x1440@60Hz";
-    };
-  };
+  services.batteryAlert.enable = true;
 };
