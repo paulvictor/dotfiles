b9fabf126c5ce5fb171eef36b760120e06601e9f
Paul Victor Raj <paulvictor@gmail.com>
2026-02-25 11:47:06 +0530

    Removing kanata from anarki

--- nix/root/devices/anarki/default.nix
+++ nix/root/devices/anarki/default.nix
@@ -8,7 +8,6 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ../../modules/kanata/default.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
