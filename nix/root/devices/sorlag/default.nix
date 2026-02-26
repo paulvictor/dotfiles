19afda0025ae226da4afdc0591f2804a13e8d731
Paul Victor Raj <paulvictor@gmail.com>
2026-02-26 19:21:55 +0530

    Add tun on sorlag

--- nix/root/devices/sorlag/default.nix
+++ nix/root/devices/sorlag/default.nix
@@ -9,6 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ../../modules/kanata/default.nix
+      ../../modules/split-gp-tun/default.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
