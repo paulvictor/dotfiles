fa38d0f4cce0cb56e9d763cda9dd581433ac4ae4
Paul Victor Raj <paulvictor@gmail.com>
2026-02-27 12:14:34 +0530

    Fix for gptel

--- stow/emacs/.emacs.d/ai-coding.el
+++ stow/emacs/.emacs.d/ai-coding.el
@@ -5,8 +5,14 @@
 
 (use-package gptel
   :custom
-  ((gptel-model 'qwen3-coder-next)
-   (gptel-backend (gptel-make-ollama "Ollama"
-                    :host "localhost:11434"
-                    :stream t
-                    :models '(qwen3-coder-next)))))
+  (gptel-default-mode 'org-mode)
+  (gptel-prompt-prefix-alist nil)
+  (gptel-org-branching-context t)
+  (gptel-model 'qwen3-coder-next)
+  :config
+  (setq gptel-expert-commands t)
+  (setf (gptel-get-backend "ChatGPT") nil)
+  (gptel-make-ollama "ollama"
+    :host "localhost:11434"
+    :stream t
+    :models '(qwen3-coder-next)))
