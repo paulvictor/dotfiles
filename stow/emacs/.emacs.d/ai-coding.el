5de5e69abcd4acfc0b301e942d772f6855d96586
Paul Victor Raj <paulvictor@gmail.com>
2026-03-03 15:56:22 +0530

    WIP ai coding with emacs

--- stow/emacs/.emacs.d/ai-coding.el
+++ stow/emacs/.emacs.d/ai-coding.el
@@ -8,11 +8,29 @@
   (gptel-default-mode 'org-mode)
   (gptel-prompt-prefix-alist nil)
   (gptel-org-branching-context t)
-  (gptel-model 'qwen3-coder-next)
+  ;;   (gptel-model 'qwen3-coder-next)
   :config
   (setq gptel-expert-commands t)
   (setf (gptel-get-backend "ChatGPT") nil)
-  (gptel-make-ollama "ollama"
+  (setf gptel--known-backends nil)
+  (gptel-make-ollama "local/anarki"
     :host "localhost:11434"
     :stream t
-    :models '(qwen3-coder-next)))
+    :models '(qwen3-coder-next))
+  (gptel-make-openai "xyne"
+    :host "grid.ai.juspay.net"
+    :key (password-store-get "work/Juspay/grid.ai/sarge")
+    :stream t
+    :endpoint "/"
+    :models '(minimaxai/minimax-m2
+              glm-latest
+              claude-opus-4-5
+              kimi-latest))
+  (gptel-make-gemini "Gemini"
+    :key (password-store-get "work/Juspay/gemini/anarki")
+    :stream t)
+  (gptel-make-openai "personal-openai"
+    :key (password-store-get "openai/key/anarki")
+    :endpoint "/v1/chat/completions"
+    :stream t
+    :models '(gpt-5-nano)))
