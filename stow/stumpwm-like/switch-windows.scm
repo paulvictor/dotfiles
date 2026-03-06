a5e2deadc4a7445ddf77eaf51db739f8a6da584f
Paul Victor Raj <paulvictor@gmail.com>
2026-03-06 16:13:59 +0530

    WIP window switching

--- stow/stumpwm-like/switch-windows.scm
+++ stow/stumpwm-like/switch-windows.scm
@@ -1,6 +1,7 @@
 (use-modules (ice-9 pretty-print)
              (libs sway-tree-helper)
              (srfi srfi-1)
+             (ice-9 rdelim)
              (swayipc))
 
 (define* (window-list pred #:optional (tree (sway-get-tree)))
@@ -13,10 +14,22 @@
           new-l)))
   (fold acc-nodes '() (sway-tree-nodes tree)))
 
-(define (focusable-windows)
+(define (focusable-windows-and-ids)
   (map
    (lambda (n) (cons (sway-tree-name n) (sway-tree-id n)))
    (window-list
     (lambda (n) (and
                  (string= (sway-tree-type n) "con")
                  (not (eq? 'null (sway-tree-name n))))))))
+
+(define (get-choice)
+  (let* ((choice-cmd "fuzzel --dmenu")
+         (choices (focusable-windows-and-ids))
+         (focusable-windows (map car choices))
+         (window-choice-str (string-join focusable-windows "\n" 'suffix))
+         (selected (let ((port
+                           (open-input-output-pipe choice-cmd)))
+                     (display window-choice-str port)
+                     (read-line port))))
+    (pretty-print window-choice-str)
+    selected))
